484eb343cb6448f4be90def9f86cbe78b41428cb
Shawn Friedel <shawn@nulogix.net>
2023-06-03 20:18:00 -0400

    Update mxslimmer.sh

--- mxslimmer.sh
+++ mxslimmer.sh
@@ -2,7 +2,7 @@
 
 echo MX Slimmer v1.0
 
-for package in libreoffice* vlc* yubi*; do
+for package in gnome-mahjongg* lbreakout* libreoffice* peg-e swell-foop vlc* yubi*; do
   sudo apt-get purge -y $package
 done 
 
